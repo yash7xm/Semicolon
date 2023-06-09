5113f3fa210834cb27699abe84fd24b499373fc0
yash7xm <yash.7xm@gmail.com>
2023-06-09 11:26:39 +0530

    heading text fixed

--- scripts/front-page.js
+++ scripts/front-page.js
@@ -67,9 +67,9 @@ function handleMouseEnter() {
     const headingSecondaryText = document.querySelector(`[data-index="${activeIndex}"] .heading-secondary-text`);
     headingFrontText.style.opacity = '0';
     headingSecondaryText.style.opacity = '1';
-    scrollTopStop = true;
+    headingFrontText.style.transform = 'translateY(-100%)';
+    headingSecondaryText.style.transform = 'translateY(-100%)';
     mouseCaret.classList.add('caret-grow-heading');
-    scrollToBottom(headingText.scrollTop, headingText.scrollHeight, headingText);
     rotatingIcon.style.transform = "rotate(90deg)";
     rotatingIcon.style.transition = "transform 0.3s ease-out";
 }
@@ -81,40 +81,13 @@ function handleMouseExit() {
     const rotatingIcon = document.querySelector(`[data-index="${activeIndex}"] .heading .rotating-icon img`);
     headingFrontText.style.opacity = '1';
     headingSecondaryText.style.opacity = '0';
-    scrollDownStop = true;
+    headingFrontText.style.transform = 'translateY(0%)';
+    headingSecondaryText.style.transform = 'translateY(100%)';
     mouseCaret.classList.remove('caret-grow-heading');
-    scrollToTop(headingText.scrollTop, 0, headingText);
     rotatingIcon.style.transform = "rotate(0deg)";
     rotatingIcon.style.transition = "transform 0.3s ease-out";
 }
 
-
-function scrollToBottom(start, end, headingText) {
-    let scrolled = start;
-    let scrollEnd = end;
-    const scrollInterval = setInterval(() => {
-        headingText.scrollTop = scrolled;
-        scrolled += 1.5;
-        scrollTopStop = false;
-        if (scrollDownStop || scrolled >= scrollEnd) {
-            console.log(scrolled);
-            clearInterval(scrollInterval);
-        }
-    }, 1);
-}
-
-function scrollToTop(start, end, headingText) {
-    let scrolled = start;
-    let scrollEnd = end;
-    const scrollInterval = setInterval(() => {
-        headingText.scrollTop = scrolled;
-        scrolled -= 1.5;
-        scrollDownStop = false;
-        if (scrollTopStop || scrolled <= scrollEnd) {
-            clearInterval(scrollInterval);
-        }
-    }, 1);
-}
 
 function handleRightBtn() {
     const nextIndex = activeIndex + 1 <= slides.length - 1 ? activeIndex + 1 : 0;
